cf4b753da7e9a569b32b8e0a62f91d669d8a710b
Lance Borden <103229880+BanceDev@users.noreply.github.com>
2024-08-01 15:15:34 -0400

    Lua ECS prototype

---
+++ table/scripts/entity.lua
@@ -0,0 +1,20 @@
+local Entity = {}
+Entity.__index = Entity
+
+function Entity:new(id)
+    return setmetatable({id = id, components ={}}, self)
+end
+
+function Entity:addComponent(componentName, component)
+    self.components[componentName] = component
+end
+
+function Entity:getComponent(componentName)
+    return self.components[componentName]
+end
+
+function Entity:removeComponent(componentName)
+    self.components[componentName] = nil
+end
+
+return Entity
---
+++ table/scripts/world.lua
@@ -0,0 +1,16 @@
+-- not yet implemented but this is
+-- where the entry point for execution will be
+
+function worldSpawn()
+-- create all entities and systems 
+-- (should just be every system) that 
+-- will exist at game start
+end
+
+function main()
+    worldSpawn()
+-- updates
+
+-- draws
+
+end
